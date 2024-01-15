18b43cb1d864f6c1091be0324da5e6c1e192ea6a
Ivan Dachev <i_dachev@yahoo.co.uk>
2024-01-15 20:07:29 +0200

    Fixed

--- linux/disable_usb_mouse_wakup.sh
+++ linux/disable_usb_mouse_wakup.sh
@@ -1,10 +1,17 @@
 https://unix.stackexchange.com/questions/417956/make-changes-to-proc-acpi-wakeup-permanent/532839#532839
+https://askubuntu.com/questions/252743/how-do-i-prevent-mouse-movement-from-waking-up-a-suspended-computer
 
 For a USB mouse or keyboard, you can use a udev rule to make the setting permanent. First, look up the PCI vendor ID of your mouse/keyboard using lsusb. For my mouse, it's 046d:
 
 Bus 001 Device 006: ID 046d:c52b Logitech, Inc. Unifying Receiver
 
-Then create a "rules" file like my /etc/udev/rules.d/logitech.rules, only replace "046d" with the vendor ID of your own device:
+Then create a "rules" file like:
+/etc/udev/rules.d/20-logitec.rules
 
-ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTR{power/wakeup}="disabled"
+Replace "046d" and "c52b" with the vendor ID/product ID of your own device:
+
+ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c52b", ATTR{power/wakeup}="disabled"
+
+After latest update 2024.01 need to do:
+
 
